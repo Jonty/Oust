feedd66fd77bf593f4347bc7dc93803083bac30c
Jonty Wareing <jonty@jonty.co.uk>
2016-05-21 18:52:48 +0100

    Fix new controllers pairing/joining with bluez5
    When a brand new controller is paired the bluetooth stack is restarted
    by psmoveapi. In previous versions of bluez this wasn't a problem,
    however in bluez5 (shipped in the latest raspbian) the bluetooth device
    power state is not persisted across restarts - udev activates it on
    boot, but if you /etc/init.d/bluetooth restart the adapter will not be
    powered on.
    
    This means that whenever you connect a brand new controller over USB to
    pair it you have to cycle the rpi to get the bluetooth re-enabled.
    
    This changes scan_enable to also power up the devices constantly,
    working around the problem. Ugly, but whatever.

--- scan_enable.sh
+++ scan_enable.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 set -eo pipefail
 
-devices=$(hcitool dev | grep hci | awk '{print $1}')
+echo "Watching hci devices..."
 
-for device in $devices
-do
-    scan_enable=$(hcitool -i ${device} cmd 0x3 0x19|grep -A1 'HCI Event'|tail -1|cut -d\  -f7)
-    if [[ "$scan_enable" == '00' ]]; then
-        echo "Scan disabled, enabling"
-        # Could also use hciconfig hci0 pscan
-        success=$(hcitool -i ${device} cmd 0x3 0x1a 0x2|grep -A1 'HCI Event'|tail -1|cut -d\  -f6)
-        if [[ "$success" != '00' ]]; then
-            echo "Scan enable failed"
-            exit 1
+while true; do
+    devices=$(hciconfig dev | grep hci | awk '{print $1}' | sed -e 's/://')
+
+    for device in $devices
+    do
+        # Force the device to be up, bluez 5.0 does not remember device power
+        # state through restarts
+        hciconfig $device up
+
+        scan_enable=$(hcitool -i ${device} cmd 0x3 0x19|grep -A1 'HCI Event'|tail -1|cut -d\  -f7)
+        if [[ "$scan_enable" == '00' ]]; then
+            echo "Scan disabled, enabling"
+            # Could also use hciconfig hci0 pscan
+            success=$(hcitool -i ${device} cmd 0x3 0x1a 0x2|grep -A1 'HCI Event'|tail -1|cut -d\  -f6)
+            if [[ "$success" != '00' ]]; then
+                echo "Scan enable failed"
+                exit 1
+            fi
+            echo "Scan now enabled for ${device}"
         fi
-        echo "Scan now enabled for ${device}"
-    else
-        echo "Scan already enabled on ${device}"
-    fi
+    done
+
+    sleep 1
 done
