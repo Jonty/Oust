8254996a9e8fbe8778f2e18d332d3b550a87bbd9
Jonty Wareing <jonty@jonty.co.uk>
2016-12-29 15:17:09 +0100

    Don't bail when we can't enable scanning
    This eventually causes supervisor to stop the process when really we
    just want to try again when the adapter stops being silly.

--- scan_enable.sh
+++ scan_enable.sh
@@ -10,16 +10,16 @@ while true; do
     do
         # Force the device to be up, bluez 5.0 does not remember device power
         # state through restarts
-        hciconfig $device up
+        hciconfig "${device}" up
 
-        scan_enable=$(hcitool -i ${device} cmd 0x3 0x19|grep -A1 'HCI Event'|tail -1|cut -d\  -f7)
+        scan_enable=$(hcitool -i "${device}" cmd 0x3 0x19|grep -A1 'HCI Event'|tail -1|cut -d\  -f7)
         if [[ "$scan_enable" == '00' ]]; then
             echo "Scan disabled, enabling"
             # Could also use hciconfig hci0 pscan
-            success=$(hcitool -i ${device} cmd 0x3 0x1a 0x2|grep -A1 'HCI Event'|tail -1|cut -d\  -f6)
+            success=$(hcitool -i "${device}" cmd 0x3 0x1a 0x2|grep -A1 'HCI Event'|tail -1|cut -d\  -f6)
             if [[ "$success" != '00' ]]; then
                 echo "Scan enable failed"
-                exit 1
+                break
             fi
             echo "Scan now enabled for ${device}"
         fi
